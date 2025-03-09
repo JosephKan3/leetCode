4d9c67afc8ca92ad8735ff95d0bd8a07d9ffd24d
DrainCena <josephkan3@gmail.com>
2025-03-08 22:05:43 -0500

    feat: adds solution 45

---
+++ 45/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <sstream>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+  public:
+      int jump(vector<int>& nums) {
+          vector<int> dp(nums.size(), INT_MAX);
+
+          dp[0] = 0;
+          for (int i = 0; i < nums.size(); i++) {
+            // Can jump to from i
+            int max_val = min(i + nums[i] + 1, static_cast<int>(nums.size()));
+            for (int j = i + 1; j < max_val; j++) {
+              if (dp[i] != INT_MAX) {
+                dp[j] = min(dp[i] + 1, dp[j]);
+              }
+            }
+          }
+
+          return dp[nums.size() - 1];
+      }
+  };
+
+template <typename T>
+string vectorToString(const vector<T> &vec, const string &delimiter = ", ")
+{
+  ostringstream oss;
+  for (size_t i = 0; i < vec.size(); ++i)
+  {
+    if (i > 0)
+      oss << delimiter; // Add delimiter between elements
+    oss << vec[i];
+  }
+  return oss.str();
+}
+
+template <typename T>
+string gridToString(const vector<vector<T>> &grid, const string &rowDelimiter = "\n", const string &colDelimiter = ", ") 
+{
+  ostringstream oss;
+  for (size_t i = 0; i < grid.size(); ++i) 
+  {
+    if (i > 0)
+      oss << rowDelimiter; // Add row delimiter between rows
+
+    for (size_t j = 0; j < grid[i].size(); ++j) 
+    {
+      if (j > 0)
+        oss << colDelimiter; // Add column delimiter between elements
+      oss << grid[i][j];
+    }
+  }
+  return oss.str();
+}
+
+
+struct TestCase
+{
+  vector<int> nums;
+};
+
+int main()
+{
+  Solution solution;
+  vector<TestCase> test_cases = {
+    {{2,0,2,0,1}},
+    {{2,3,1,1,4}},
+    {{2,3,0,1,4}},
+  };
+
+  for (auto &test_case : test_cases)
+  {
+    auto result = solution.jump(test_case.nums);
+    cout << "k: " << vectorToString(test_case.nums);
+    cout << "\nResult: " << result << "\n\n";
+  }
+
+  return 0;
+}
